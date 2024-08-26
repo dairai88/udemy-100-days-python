f996233ad122443d37bb7c2193b9a11339f32995
Dalei Sun <sundalei2011@163.com>
2024-08-26 23:14:55 +0800

    snake game add food

--- snake_game/main.py
+++ snake_game/main.py
@@ -2,6 +2,7 @@
 from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen() # pylint: disable=invalid-name
 screen.setup(width=600, height=600)
@@ -10,6 +11,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,5 +25,9 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
