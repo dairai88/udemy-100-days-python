4ec2621cc7aba956c4d60da53541bead18dc9e1a
Dalei Sun <sundalei2011@163.com>
2024-09-09 23:05:21 +0800

    pandas usage

---
+++ day-25/main.py
@@ -0,0 +1,29 @@
+"""main"""
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(len(temp_list))
+
+print(data['temp'].mean())
+print(data['temp'].max())
+
+# Get Data in Columns
+print(data["condition"])
+print(data.condition)
+
+# Get Data in Row
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
+
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data = pandas.DataFrame.from_dict(data_dict)
+data.to_csv("new_data.csv", index=False)
