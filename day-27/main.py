63776a6bf5676b92b6c3a55b1b85f4cb8716204f
Dalei Sun <sundalei2011@163.com>
2024-09-26 21:55:58 +0800

    tkinter day 27

--- day-27/main.py
+++ day-27/main.py
@@ -1,18 +1,6 @@
 """day 27"""
 from tkinter import Tk, Label, Button, Entry, END
 
-# Creating a new window and configurations
-window = Tk()
-window.title("Widget Examples")
-window.minsize(width=500, height=500)
-
-# Label
-my_label = Label(text="This is old text", font=("Arial", 24, "bold"))
-my_label.config(text="This is new text")
-my_label.pack()
-
-# Button
-
 
 def button_clicked():
     """button clicked"""
@@ -21,15 +9,32 @@ def button_clicked():
     my_label.config(text=new_text)
 
 
+# Creating a new window and configurations
+window = Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+window.config(padx=40, pady=40)
+
+# Label
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+my_label.config(padx=20, pady=20)
+
+# Button
+
 # calls action() when pressed
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
 
 # Entry
 textInput = Entry(width=30)
 # Add some text to begin with
 textInput.insert(END, string="Some text to begin with.")
 print(textInput.get())
-textInput.pack()
+textInput.grid(column=3, row=2)
 
 window.mainloop()
